e5f3c3ae15c17a2222950c33c56518b2ffb03ac3
AramysMatos <aramysmatos@poli.ufrj.br>
2016-11-03 01:48:24 -0200

    add project

---
+++ paginaIndex.js
@@ -0,0 +1,335 @@
+window.onload = iniciar;
+
+var controle;
+
+var dialogo;
+var loveletter = ["t", "w", "j", "p", "x"];
+var letter = "qwtrbnvmczspkjgxdflh";
+
+function iniciar() {
+    
+    
+    var texto = "wfslhk slct slmzpg wdnkrx jxjc bppxs wbv ttgv pzxxd zqt fgmbxrd nrghqcp nqldnxz cscrnmzw bnvxjht clnckddt cdcvxjw mqplbpx scqc wtvwhj cwjw rkqp gvjrq jwvsjslq mkq kcsgk rdv gbjh fzwq vdxzrm znbjxqf mgxlrk gnn kngdxfcp rvjjhp jrm wnphjqfv dhr lppqxzfj kkbgbrm dpbg xtp mpwtn fghksjh kvlvnxb wvpvcbw mmsmnm lzccq fmjrnmn dllc brjf mmcl ffbhgnk jqcgkls nldbr ffkx mcvqt prnprxxd nxqr nxmrf mzklwvs kwxhfw jdcmszhh hsmxxhdw jnjmqgx rsrbvkt kbpv ffrbgl rhkkmhv bcfkvfj rtcmx mbjvq ftg fgfwbcrh rdkglj qqmsnz dnqb tkfhmwt gpmlfq fkh fftrmdgd vctsff hrmnbd smkqb mrrdxfrn xvlljclx blnbv nsw jzxlssg ljrz kbklpnp zkpcj kgct znvqbd ffksdqbj vrnc hqsz mlrkmccx xptmm sjlgxw qznt hqxts ssd dndcj hslhm slpqxk xvqzvw xlr zglqwb mxcgt nhhh zfwlmqfk jjm jcqg wjfgrccm thmmtx kjrjjdtp ksgx bxbm fqcm clmh vtwkwg ncrfsjhw pbhnwxd klnnlxv kpwd dxldszgn whhg bpxh sqwdtnnz wffwkmdm brzzd hgdvl vszpkmph gtqwpg kszkmp rlzgpfx lwglvt bzmsrrnn vgl nnpn dvfrllx jcj ccstglq kvngkjv snvh fhwqtk wmwdhq bklpm xxwvrjgg nzksndv cldshlc snkkzdgd ndjk lws djcfzpth tjh xtklgs krwqw qhzvjgkb ppfwwfg cxrbmmv jxdqcdvk mpbfhn xmhqfkjh cnlzkn llp btzmzz ndgjcm cjdfm zzszd xrq xzvbdkf rvjwxbc skggk dvjwpvf pcmtxsg mgrfjvb ztmk wrjwstw bxccp sqrxtw lbkww vpbmpsl lfsdkcfh bmzdfldh vjzhh jfx knxbl ksm whgwbs ffwgps vzgghfs cwltlz jzhh rsqhq wmdxht nwfscz kmhrgv pmjlf zrtlnqj sjkrzvcb ppstn mdjtrw jtzqm bnv dmn vcpjdnvr dtbrsz smcvhgx ssbrn bdpk gdm qnpxdmh vrstmtxf rplvnnv lptm cgmwltx vnlh cdkfsqgb zrzqvpvl cbvztjt fwgbmv rsq tkgmgpgz cfmjqbw hhtlb czbx xhfstbp bqmhq mcll qcqlkqxh ddnnzmd wmqpknxv rtfnpt vlj mrtdlp tnlxsv lwr dnsscxwx sxzf lqhwtkcw qmf tpbmqmr whx gwzkqthv ggcwcld hqjt shvsz xwzgcbxc cqtp qbprvb wlr sgbwbffj dtdwd gmsvlhz bkpgkqj qbgvqmz xbbmqcx dwcxrw gqfzxhm dzcmb nkzfr hnvvpxxm qxzz nnxvw dvgz pwmnh lpqvpz jnz lrwxvx khh lnpxlds fcxzk kpdb plkvvd xtwwr nrgfjsl dprjfplf mqzpn zpvd qrz nmzzx ccwhm lpmrl gtqrjn ssqmq gzrkjstk gdc wrbbqql vqwzdxbr npv rxqw mbmqzv mnbncv jmrrm dwjkv wlxg npgcb klzwkm znxql zbg btb bplsh xmxl plqlwhzh nlbwlpdt jtzbn mdvslnw rhrcvxhk qhvd rswtf cksssmjt pnn czj xkm lmv plwd kmckd ztfx nkzx wltfpj gdsc jmltszsw zvnvm dhd tsclv dzkdj hmpmjxf qstfxh xctjh tht wgh lzjbckr dmblrlmb jxfk tjg jxc qpg rvmxfq crjkbdl kxhpjb fgkcgwjh pwfw zccc wvv bxq bgdxfb lbtmrmv bpb jtvjbwct hmqwrnt qgpbzsn wdhgfb bwh vrq dbfj vfxsv qbqwbx wtd qqzmlmrw zbl xjmfjtw qlfkxlk ggfnbhzc cvrs jbchg wqwfff cnrljfp brvljmjg brb mtkxnjl xwwnfbh rtqvr qhzhz mhlsfk tfdgv bpw dnwq fljqkp tbqd txgf qcs dgklqgrq qfm pfmfxbmt ptbtgkw prmtv lfkbtrj tqc hkc rqbg mjgqntg dxnph ktgq mqtnctb fxkvsnkj jdt hbrgckkn qtblptvw rgpgbjdm cgzv cmrrrrr rvztsr wrprlfj crvwkzf xkzt clvrcd dqnqr tsmfxhhb mwfqnp phf pqwpgbb lrbr gszz sghld znqtwz dkkkfhr rvd rtmc swdd cvrpxh jfkr dcmslljx pwmcbvnd lffgltzq tgr fvjkklmc zkjxdtjc ntf nqbsj swx bpnqv fbg ddrf srcjf zlfd jbck vzkvbd gkndtfbw smq sms zxdghtdb gqwhb nrtl mwdvdxs stvntw xhwpbbh bgwshj rpqwfbl zph wmzzlfzt njvkn mclc crspz ccfc bwbxpx rgzqwkcs lnnclj sbqqc rvdr vcjjbx gvf gqrhz znml jlhlts tplrlt gxbc qwrj hnfqszjc gvl tfxd hfstzw whbpsmzw drz cxqdn lnpvwgp dlslvr cxtf bvkxz jqkqc xxxc mzsmxfmh nhxhr plcjs cwrrtfp grvfpw qvtbptw thpg qrvhqcrj klz wsltjrpg bcrgmxdx gklqvv bplvmlkk slqbzbwf wvmvnd xzbdbks dndhblt psd ncq dnnvflzd cxl lzl spzgrq gdn xfszg xxptpd ffrjctnr jdcqvt pnjhnrv wpzm zllsjksn lfcst qxkw qzhsq bplsdsqj thvwvf dnxzkx rmx mtgqg grrjd mngxjp fwgxdb hjkp hzh kgw fvpqj mlx pdwtggbs jvk tbrlldjp dzhdn fpdvshhv xtn jlkh jdjqpxl xkxm lxhcmtf xrs gqhp jww kbpf tsltgz qfm wdbxshhs dvdndt wmr ctbttqbl btjccvt pbdmll cjn skrlsbb crqglpq xhqsh lvv rwlpj qxrsdj bwwqwz czqcr sfshjz tjtmnsmf hckg txfnhcx qqmc snwjw kqbkjwb khwcn glt zbcttf tlxvpl fdv vfb ndwvpv szsgpdjj ktrwnbmd qpqp cnht jmtcglg mrbmxxk nsqrk qffmh wswzqz lqp cmxfjlsx vhgzznwq dpzcms frgdg njc wpqhtnz stqhgqh txkn cknrbwlk rdx qqpnd gwsq jcsd jpq pkv rssfbtzh";
+
+    document.getElementById('idTextoPergaminho').innerHTML = texto; // inicia a caixa de texto com o texto do pergaminho
+
+    // prepara a resposta da pergunta 1  
+    document.getElementById('idPergunta1').addEventListener("click",
+            function () {
+                var words = [];
+                var text = "";
+                var count = 0;
+                text = document.getElementById('idTextoPergaminho').value;
+                words = extractWords(text);
+
+
+                for (var i = 0; i < words.length; i++) {// percorre a lista de palavras
+
+                    if (isPreposition(words[i])) { // verifica se é uma preposição 
+                        count++;   // soma em um contador
+                    }
+
+                }
+
+                document.getElementById('idResposta1').innerHTML = "Resposta: Existem <b>" + count + "</b> preposições no texto."; // adiciona a resposta no html
+            }
+    );
+     // prepara a resposta da pergunta 2
+    document.getElementById('idPergunta2').addEventListener("click",
+            function () {
+                var words = [];
+                var text = "";
+                var count = 0;
+                text = document.getElementById('idTextoPergaminho').value;
+                words = extractWords(text);
+
+                for (var i = 0; i < words.length; i++) {
+
+                    if (isVerb(words[i])) { //verifica se é um verbo
+                        count++;
+                    }
+
+                }
+
+                document.getElementById('idResposta2').innerHTML = "Resposta: Há <b>" + count + "</b> verbos no texto.";
+            }
+    );
+
+    // prepara a resposta da pergunta 3
+    document.getElementById('idPergunta3').addEventListener("click",
+            function () {
+                var words = [];
+                var text = "";
+                var count = 0;
+                text = document.getElementById('idTextoPergaminho').value;
+                words = extractWords(text);
+                
+
+                for (var i = 0; i < words.length; i++) {
+
+                    if (isVerb(words[i])) { // verifica se a palavra é um verbo
+                        if (isFirstPerson(words[i])) { // verifica se o verbo está na primeira pessoa
+                            count++;
+                        }
+                    }
+
+                }
+
+                document.getElementById('idResposta3').innerHTML = "Resposta: Há <b>" + count + "</b> verbos em primeira pessoa no texto.";
+            }
+    );
+
+    // prepara a resposta da pergunta 4
+    document.getElementById('idPergunta4').addEventListener("click",
+            function () {
+                var words = [];
+                var text = "";
+                var count = 0;
+                var response = "";
+                text = document.getElementById('idTextoPergaminho').value;
+                words = extractWords(text); // extrai as palavras
+                words = wordsDistinct(words); // remove valores duplicados
+                words = selection_sort(words); // organiza as palavras segundo o alfabeto predator 
+                 response += "<table>";
+
+                for (var i = 0; i < Math.ceil(words.length / 3); i++) { // forma a resposta para aparecer em em 3 colunas
+                    response += " <tr> ";
+
+                    response += " <th> " + i + ". " + words[i] + " </th>";
+                    if (i + Math.ceil(words.length / 3) < words.length) {
+                        response += " <th> " + (i + Math.ceil(words.length / 3)) + ". " + words[i + Math.ceil(words.length / 3)] + " </th>";
+                    }
+                    if (i + (Math.ceil(words.length / 3) * 2) < words.length) {
+                        response += " <th> " + (i + (Math.ceil(words.length / 3) * 2)) + ". " + words[i + (Math.ceil(words.length / 3) * 2)] + " </th>";
+                        response += "</tr>";
+                    }
+
+                }
+
+                response += "</table>";
+
+                document.getElementById('idResposta4').innerHTML = response;
+            }
+    );
+    // prepara a resposta da pergunta 4
+    document.getElementById('idPergunta5').addEventListener("click",
+            function () {
+                var words = [];
+                var text = "";
+                var count = 0;
+                text = document.getElementById('idTextoPergaminho').value;
+                words = extractWords(text); //extrai as palavras 
+                words = wordsDistinct(words); // remove valores duplicados
+
+                for (var i = 0; i < words.length; i++) {  //percorre a lista de palavras
+                    if (numberIsBeautiful(strToInt(words[i]))) { //verifica se o número é um número bonito
+                        count++; // soma em um contador
+                    }
+
+                }
+
+                document.getElementById('idResposta5').innerHTML = "Resposta: No texto, há <b>" + count + "</b> números bonitos distintos.";
+            }
+    );
+
+    // prepara a resposta da pergunta 6
+    document.getElementById('idPergunta6').addEventListener("click",
+            function () {
+                var words = [];
+                var text = "";
+                var count = 0;
+                text = document.getElementById('idTextoPergaminho').value;
+                words = extractWords(text);  //extrai as palavras 
+                words = wordsDistinct(words); // remove valores duplicados
+
+                for (var i = 0; i < words.length; i++) { //percorre a lista de palavras
+
+                    if (numberIsPretty(strToInt(words[i]))) { //verifica se o número é um número lindo
+                        count++;                                // soma em um contador
+                    }
+
+                }
+
+                document.getElementById('idResposta6').innerHTML = "Resposta: No texto, há <b>" + count + "</b> números lindos distintos"; // adiciona a resposta no html
+            }
+    );
+
+}
+
+function isPreposition(word) {   //verifica se uma string é uma preprosição 
+
+    if (word.length != 3) {
+        return false;
+    } else {
+        if (word.search("h") != -1) {
+            return false;
+        } else {
+            for (var i = 0; i < loveletter.length; i++) {
+                if (word.charAt(word.length - 1) == loveletter[i]) { // verifica se a ultima letra é do tipo love
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+    }
+
+
+}
+
+function isVerb(word) {    // verifica se a string é um verbo 
+
+    if (word.length < 6) {
+        return false;
+    } else {
+        for (var i = 0; i < loveletter.length; i++) {
+            if (word.charAt(word.length - 1) == loveletter[i]) { // verifica se a ultima letra é do tipo love
+                return false;
+            }
+        }
+        return true;
+
+
+    }
+
+
+}
+
+function isFirstPerson(verb) {  // verifica se o verbo está na primeira pessoa    
+
+    for (var i = 0; i < loveletter.length; i++) {
+        if (verb.charAt(0) == loveletter[i]) { // verifica se a primeira letra é do tipo love
+            return true;
+        }
+    }
+    return false;
+
+}
+
+function smaller(word1, word2) { // verifica qual string é menor seguindo os critério o alfabeto predator
+    length_min = Math.min(word1.length, word2.length);
+
+
+
+    for (var n = 0; n < length_min; n++) {
+
+        if (letter.indexOf(word1.charAt(n)) < letter.indexOf(word2.charAt(n))) {
+            return true;
+        } else {
+            if (letter.indexOf(word1.charAt(n)) > letter.indexOf(word2.charAt(n))) {
+                return false;
+            }
+
+
+        }
+
+    }
+    if (word1.length < word2.length) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+function extractWords(text) { // extrai as palavras do texto e guarda em uma lista
+    words = [];
+    words = text.split(" ");
+    return words;
+
+}
+
+
+
+function strToInt(word) { // transforma um texto em um valor numerico da base 20 convitido para a base 10
+    var number = 0;
+
+    for (var i = 0; i < word.length; i++) {
+
+        number += Math.pow(20, i) * letter.indexOf(word.charAt(i));
+
+    }
+    return number;
+}
+
+function numberIsBeautiful(number) {
+
+    if ((number >= 623732) && (number % 4 == 0)) {  // verifica o critério que define se um número é bonito     
+        return true;
+    }
+    return false;
+}
+
+
+
+function countDigits(number) {
+    var count = 0;
+    while (number > 0) {
+        count += number % 10; //pega o resto da divisão por 10, já que esse é o valor do dígito       
+        number = Math.floor(number / 10);
+    }
+
+    return count;
+
+}
+
+
+
+function isPrime(number) {
+    for (var i = 2; i < number; i++) {
+        if (number % i == 0) {   //verifica se o resto da divisão é 0 para cada numero entre 2 e o numero de entrada -1         
+            return false;
+        }
+    }
+    if ((number == 0) || (number == 1)) {
+        return false;
+    } else {
+        return true;
+    }
+
+}
+
+function numberIsPretty(number) {
+    return isPrime(countDigits(number));
+}
+
+function wordsDistinct(words) {
+    var listwords = [];
+    for (var i = 0; i < words.length; i++) { // percorre a lista de palavars
+        if (listwords.indexOf(words[i]) == -1) { //verifica se a palavra já se encontra na lista
+
+            listwords.push(words[i]); //Se a palavra não se encontra na lista, ela é adicionada para a primeira posição
+        }
+    }
+    return listwords;
+}
+
+
+function selection_sort(words) {
+    var min = 0;
+    var aux = "";
+
+    for (var i = 0; i < words.length - 1; i++)
+    {
+        min = i;
+        for (var j = (i + 1); j < words.length; j++) {
+            if (smaller(words[j], words[min])) { //verifica qual é o menor levando em consideração a ordem do alfabeto em predator "qwtrbnvmczspkjgxdflh"
+                min = j;
+            }
+        }
+        if (i != min) {
+            //realiza a troca de valores entre as duas posições
+            aux = words[i];
+            words[i] = words[min];
+            words[min] = aux;
+        }
+    }
+
+    return words;
+
+}
+
+
+
+
